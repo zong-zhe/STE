1ea0a589e7a9fa3bbdb8f1f851632b2c6707ddf9
zong <zongzhe1996@163.com>
2020-08-04 16:11:09 +0800

    Add test set

--- src/test/java/cn/edu/nuaa/ste/TestSuite.java
+++ src/test/java/cn/edu/nuaa/ste/TestSuite.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
+//测试集
 @Suite.SuiteClasses({
         BubbleSortServiceTest.class,
         ChooseSortServiceTest.class,
